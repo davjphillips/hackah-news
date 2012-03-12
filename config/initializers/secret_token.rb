a007172bf9dc7f61410b119095aa86542fd288ba
David J. Phillips <D@sutterstudios.com>
2012-03-12 10:58:23 -0700

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackahNews::Application.config.secret_token = 'ec19adee45f28e4d3af22bc6ad7debf9f2b496400229a07ee4f34292a23a30917459015bf6b4477325c5ac50c2ab02228a5ae3a8161d83f8c605358392da8d0e'
